cc6cca620a396f97ac164363c74b26b029d1097c
Walkberg <samuel.bergerot@gmail.com>
2023-12-30 14:13:42 +0100

    refactor: add client interface

--- client/src/modules/contracts/contract.ts
+++ client/src/modules/contracts/contract.ts
@@ -1,3 +1,13 @@
 export interface Contract {
   id: string;
+  operationId: string;
+}
+
+export interface ContractFiltering {
+  operationId: string;
+}
+
+export interface ContractClient {
+  getContract: (id: string) => Promise<Contract>;
+  getContracts: (filtering: ContractFiltering) => Promise<Contract[]>;
 }
